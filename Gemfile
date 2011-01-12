b0e165aa719f973ee02f3732048e265c026fe02d
Peter Kordel <pkordel@gmail.com>
2011-01-12 10:03:55 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+gem 'ruby-debug19'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R3::Application.config.secret_token = '0237a0fac7bc79bb451bb4daf20f5cdbd5e7dfae1c12705c7000d916f38d0a1477f99e41f5670bf66dd900f809edd4e06948372108a702f5cd5aecf82c24138e'
